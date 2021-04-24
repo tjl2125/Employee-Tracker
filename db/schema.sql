78bd11a3b20baec0c1c8282c65968893785c7a63
Taylor Leong <tjl2125@alum.barnard.edu>
2021-04-23 22:00:24 -0400

    fixed syntax error w/ table

--- db/schema.sql
+++ db/schema.sql
@@ -10,7 +10,7 @@ CREATE TABLE department (
 );
 
 CREATE TABLE role (
-  id INT NOT NULL AUT0_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NULL,
   salary DECIMAL(10,2) NULL,
   department_id INT NULL,
